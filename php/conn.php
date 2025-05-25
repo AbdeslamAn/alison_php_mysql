bd7fc292ef332a0fd0533666f5a87b6375068b22
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-05-25 18:12:36 +0000

    Add files via upload

---
+++ php/conn.php
@@ -0,0 +1,28 @@
+<?php
+
+$dns = 'mysql:host=localhost;dbname:alison_kurs_php';
+$username='root';
+$password="";
+
+// $option= array(
+//     // for not problem mit sprechen arabisch
+//     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',);
+
+try {
+
+    $db= new PDO($dns,$username,$password,$option); 
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // $sql= "";
+    // $db->exec($sql);  
+
+
+}
+ catch (PDOException $e) {
+
+    echo " Failad ". $e-> getMessage();
+}
+
+
+
+?>
---
+++ user.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $username=htmlspecialchars($_POST["username"]);
+    $password=htmlspecialchars($_POST["password"]);
+
+    if (empty($username)) {
+        $usernameEroor= "User name is Empty!!";
+    }
+   
+    if (empty($password)) {
+        $passwordEroor= "password is Empty!!";
+    }
+    else {
+        echo "user name ist : $username <br /> Password ist : $password <br />" ;
+    }
+    
+     
+
+}
+
+    // else {
+    //     return 
+    // }
+
+?>
