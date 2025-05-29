a142ec4f8707f81ec37b926fca3dc6029ad13bc3
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-05-29 17:54:06 +0000

    Konstruktor mit Parametern / Constructor withe Parameter

---
+++ OOP/04_constructor_parameter.php
@@ -0,0 +1,26 @@
+<?php 
+
+ class Vehicle{
+
+            public $speed;
+            public $acceleration;
+            public $wheels;
+
+            public function __construct($speed,$acceleration,$wheels) {
+                $this->speed = $speed;
+                $this->acceleration = $acceleration;
+                $this->wheels = $wheels;
+                
+            }
+
+            public function allinfo() {
+                echo "Speed : ".$this->speed."<br>";
+                echo "acceleration : ".$this->acceleration."<br>";
+                echo "wheels : ".$this->wheels."<br>";
+            }
+
+            
+    }
+
+    $vehicle = new Vehicle($speed= 20,$acceleration= 5,$wheels= 6);
+    $vehicle->allinfo();
