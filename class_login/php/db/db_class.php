28a00f5dd37e7822a689a178e40ef4c70ffd762a
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-05-31 17:11:43 +0000

    Datei conn.php benennt im db_class.php um / Rename file conn.php to db_class.php im folders class_login/php/db/

---
+++ class_login/php/db/db_class.php
@@ -0,0 +1,29 @@
+<?php
+
+    class datbase{
+
+        private $connection;
+        private $servername;
+        private $username;
+        private $password;
+        private $dbname;
+
+        public function __construct($servername,$username,$password,$dbname){
+
+            $this->servername = $servername;
+            $this->username = $username;
+            $this->password = $password;
+            $this->dbname = $dbname;
+
+            try {
+                $this->connection = new PDO("mysql:host=$this->servername;dbname=$this->dbname",$this->username,$this->password);
+
+                $this->connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+                echo "Connected succssfull<br>";
+            } catch (PDOException $error) {
+                echo "Connected Filed :".$error->getMessage();
+            }
+        }
+
+    }
