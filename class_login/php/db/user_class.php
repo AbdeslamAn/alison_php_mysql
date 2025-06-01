cde509c96dc21870c5df1d7b21360000c3273ce2
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-06-01 17:48:22 +0000

    PASSWORD_DEFAULT

--- class_login/php/db/user_class.php
+++ class_login/php/db/user_class.php
@@ -11,12 +11,14 @@
 
         public function register($username,$email,$password,$confirmpassword) {
 
+            $password = password_hash($password, PASSWORD_DEFAULT);
+
             $fields = array(
                 "user_name" => array(":user_name" => $username),
                 "email" => array(":email" => $email),
                 "password" => array(":password" => $password),
             );
-
+           
             $this->db->Insert("user1",$fields);
         }
     }
